2c91319c97e76e197d79f41fee5269dd374dd2f5
Stoyan Mollov <mollov9@gmail.com>
2023-01-22 18:23:27 +0200

    update_01_FillTheMatrix

--- src/exerciseMultidimensionalArrays/_01_FillTheMatrix.java
+++ src/exerciseMultidimensionalArrays/_01_FillTheMatrix.java
@@ -12,6 +12,52 @@ public class _01_FillTheMatrix {
         String pattern = input.split(", ")[1]; // шаблон за попълване A или B.
         int[][] matrix = new int[n][n];
 
-        
+        if (pattern.equals("A")){
+            // pattern A -> обхождаме нашата матрица по колони
+            fillMatrixPatternA(matrix);
+        } else if (pattern.equals("B")) {
+            // pattern B -> по колони
+            // четна (2,4,6,8) -> от последния към първия.
+            fillMatrixPatternB(matrix);
+        }
+        printMatrix(matrix);
+    }
+
+    private static void fillMatrixPatternB(int[][] matrix) {
+        int startNumber = 1; // числа за попълване на матрицата
+        for (int col = 0; col < matrix.length; col++) {
+            if ((col + 1) % 2 ==0){
+                for (int row = matrix.length - 1; row >= 0; row--) {
+                    matrix[row][col] = startNumber;
+                    startNumber++;
+                }
+            }else {
+                for (int row = 0; row <= matrix.length - 1; row++) {
+                   matrix[row][col] = startNumber;
+                   startNumber++;
+                }
+            }
+        }
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void fillMatrixPatternA(int[][] matrix) {
+
+        int startNumber = 1;
+        // бр. редове е равен на бр. колони
+        for (int col = 0; col < matrix.length; col++) {
+            for (int row = 0; row < matrix.length; row++) {
+                matrix[row][col] =  startNumber;
+                startNumber++;
+            }
+        }
     }
 }
