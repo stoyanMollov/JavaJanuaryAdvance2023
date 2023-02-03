354b59205735f64803dcd76dd339b7bbf9249be5
Stoyan Mollov <mollov9@gmail.com>
2023-02-03 22:29:28 +0200

    update

--- src/objectsAndClasses/SpeedRacing/Car.java
+++ src/objectsAndClasses/SpeedRacing/Car.java
@@ -1,16 +1,17 @@
 package objectsAndClasses.SpeedRacing;
 
 public class Car {
-    //полета -> описваме характеристиките на нашия клас.
+    //полета -> характеристики
     private String model;
     private double fuelAmount;
     private double consumptionFuelPerKm;
     private int distanceTraveled;
 
 
-    //методите -> описват действията които ще ивършва всяка една кола.
-    //констуктор -> метод, чрез който ние съдаваме обекти от класа.
-    public Car(String model, double fuelAmount, double consumptionFuelPerKm){
+    //методи -> действия
+    //getters -> осигуряват достъп до полетата
+    //конструктор -> метод, чрез който ние създаваме обекти от класа
+    public Car(String model, double fuelAmount, double consumptionFuelPerKm) {
         //нов празен обект
         this.model = model;
         this.fuelAmount = fuelAmount;
@@ -18,20 +19,21 @@ public class Car {
         this.distanceTraveled = 0;
     }
 
-    public boolean drive(int kmDrive){
-        //1. нужно количество да измине kmDrive
+    public boolean drive(int kmDrive) {
+        // 1. нужно количечство гориво, за да преминем дадените км (kmDrive)
         double needFuel = kmDrive * this.consumptionFuelPerKm;
-        if (needFuel <= this.fuelAmount){
-            //можем да имнинем нашето разстояние
+        if (needFuel <= this.fuelAmount) {
+            //можем да изминем разстоянието
             this.fuelAmount -= needFuel;
-            this.distanceTraveled = kmDrive;
+            this.distanceTraveled += kmDrive;
             return true;
         }
         return false;
     }
+
     @Override
-    public String toString(){
-        //дава обекта под формата на текст
+    public String toString() {
+        //дава обекта под формата на текст -> "{Model} {fuelAmount} {distanceTraveled}"
         return String.format("%s %.2f %d",this.model, this.fuelAmount, this.distanceTraveled);
     }
 }
--- src/objectsAndClasses/SpeedRacing/Main.java
+++ src/objectsAndClasses/SpeedRacing/Main.java
@@ -10,36 +10,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int countCars = Integer.parseInt(scanner.nextLine());
-        Map<String,Car> cars = new LinkedHashMap<>();
-
-        for (int i = 0; i <= countCars; i++) {
+        Map<String, Car> cars = new LinkedHashMap<>();
+        //запис: модел на колата -> кола (всички характеристики)
+        //List<Car> listOfCars = new ArrayList<Car>();
+        for (int i = 1; i <= countCars; i++) {
             String[] data = scanner.nextLine().split("\\s+");
-            //"{Model} {FuelAmount} {FuelCostFor1km}"
+            //"{Model} {FuelAmount} {FuelCostFor1km}".split(" ") -> ["{Model}", "{FuelAmount}", "{FuelCostFor1km}"]
             String model = data[0];
             double fuelAmount = Double.parseDouble(data[1]);
-            double priceFuelPerKm = Double.parseDouble(data[2]);
+            double consumption = Double.parseDouble(data[2]);
 
-            //създаваме обект от конструктура.
-            Car car = new Car(model, fuelAmount, priceFuelPerKm);
-            cars.put(model,car);
-            //TODO: Къде ще съхраняваме въведените коли
+            Car car = new Car(model, fuelAmount, consumption);
+            //listOfCars.add(car);
+            cars.put(model, car);
         }
-        String command = new String(scanner.nextLine());
-        while (!command.equals("End")){
-            //"Drive {CarModel} {amountOfKm}"
+
+        String command = scanner.nextLine();
+        while (!command.equals("End")) {
+            //command = "Drive {CarModel} {amountOfKm}".split(" ") -> ["Drive", "{CarModel}", "{amountOfKm}"]
             String carModelToDrive = command.split("\\s+")[1];
             int kmToDrive = Integer.parseInt(command.split("\\s+")[2]);
 
-
-            //вьимам коя кола ще карам
+            //взимам коя кола ще карам
             Car carToDrive = cars.get(carModelToDrive);
-            if (!carToDrive.drive(kmToDrive)){
+
+            //drive -> false -> горивото не ни стига, за да изминем разстоянието
+            //drive -> true -> горивото е стигнало и ние сме изминали разстоянието
+            if (!carToDrive.drive(kmToDrive)) {
                 System.out.println("Insufficient fuel for the drive");
             }
             command = scanner.nextLine();
         }
+
         for (Car car : cars.values()) {
-            System.out.println(car);
+            //"{Model} {fuelAmount} {distanceTraveled}"
+            System.out.println(car.toString());
         }
     }
 }
