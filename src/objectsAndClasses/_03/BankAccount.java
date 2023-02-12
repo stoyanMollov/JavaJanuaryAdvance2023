55b086c4c90f1fb5674cd707df02e88b2de924b2
Stoyan Mollov <mollov9@gmail.com>
2023-02-12 09:28:21 +0200

    _01_PartyShop

--- src/objectsAndClasses/_03/BankAccount.java
+++ src/objectsAndClasses/_03/BankAccount.java
@@ -1,11 +1,19 @@
 package objectsAndClasses._03;
 
 public class BankAccount {
+    private static final double DEFAULT_INTEREST_RATE = 0.02;
+    private static double interestRate = DEFAULT_INTEREST_RATE;
     private int id;
     private double balance;
-    private double interestRate;
 
+
+    public double getInterest(int years){
+        return this.balance * interestRate * years;
+    }
+    public void deposit(double balance){
+        this.balance += balance;
+    }
     public static void setInterestRate(double interest){
-        
+        interest = interest;
     }
 }
