7ecebbf843910de25c7c0a808fc345718d1eea56
stoyanMollov <mollov9@gmail.com>
2023-02-13 11:20:18 +0200

    _02_snake

---
+++ src/examPreparation_01/_02_Snake.java
@@ -0,0 +1,11 @@
+package examPreparation_01;
+
+import java.util.Scanner;
+
+public class _02_Snake {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
