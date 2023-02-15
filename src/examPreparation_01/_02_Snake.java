7a2a9c45535674ec6cc657c179bed52c82d93d33
Stoyan Mollov <mollov9@gmail.com>
2023-02-15 08:20:40 +0200

    update

--- src/examPreparation_01/_02_Snake.java
+++ src/examPreparation_01/_02_Snake.java
@@ -6,6 +6,13 @@ public class _02_Snake {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
+
+        char[][] field = new char[n][];
+
+        for (int i = 0; i < field.length; i++) {
+            field[i] = scanner.nextLine().toCharArray();
+        }
         
     }
 }
